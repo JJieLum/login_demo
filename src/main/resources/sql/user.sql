384ffe1b41a5088e961a3c5f9fce60811eb780c8
jielum <jiankealu@qq.com>
2023-10-13 16:51:27 +0800

    fix: 更新SQL

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -4,7 +4,8 @@ create table user
     id        int auto_increment comment '数据标识'
         primary key,
     user_name varchar(50)  not null comment '用户名',
-    password  varchar(255) not null comment '密码'
+    password  varchar(255) not null comment '密码',
+    phone  bigint not null comment '手机号'
 )
     comment '用户表' row_format = DYNAMIC;
 
